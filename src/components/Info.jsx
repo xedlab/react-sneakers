21d19d48e44f6dfa8f8031895d57a7d00a4f7b7e
sparmira <aliceops09@gmail.com>
2025-10-25 00:01:25 +0600

    fix: remove .env from tracking and add to .gitignore

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,21 +2,13 @@ import React from "react";
 import { AppContext } from "../App";
 
 export default function Info({ image, title, description }) {
-const { setCartOpened } = React.useContext(AppContext)
+  const { setCartOpened } = React.useContext(AppContext);
 
   return (
     <div className="cartEmpty">
-      <img
-        className="mb-20"
-        width={120}
-        
-        src={image}
-        alt=""
-      />
+      <img className="mb-20" width={120} src={image} alt="" />
       <h2>{title}</h2>
-      <p className="opacity-6">
-       {description}
-      </p>
+      <p className="opacity-6">{description}</p>
       <button onClick={() => setCartOpened(false)} className="backGreenButton">
         <img src="/img/arrow.svg" alt="." />
         Вернутся назад
